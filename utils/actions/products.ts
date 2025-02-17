41bc7a45a675c553a91146d47a58b0086e885016
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-17 22:32:47 +0700

    add: function for fetching single product

--- utils/actions/products.ts
+++ utils/actions/products.ts
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import db from "@/utils/config/db";
 
 export const fetchFeaturedProducts = async () => {
@@ -10,12 +11,30 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = async () => {
-  const products = await db.product.findMany({
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
+  const products = db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
   });
 
   return products;
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) {
+    redirect("/products");
+  }
+  return product;
 };
