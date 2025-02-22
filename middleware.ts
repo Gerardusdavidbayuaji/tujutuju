946c2581328159f46c734e6e95533ad3974d33db
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-22 09:30:18 +0700

    refactor: admin router

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
+const isAdminRouter = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
+  const isAdminUser = userId === process.env.ADMIN_USER_ID;
+
+  if (isAdminRouter(req) && !isAdminUser) {
+    return redirectToSignIn({ returnBackUrl: "/" });
+  }
 
   if (!isPublicRoute(req) && !userId) {
     return redirectToSignIn();
