55ad254deef966088b2a97a4503ad8dc29323e5b
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-22 09:31:17 +0700

    feat: add admin router

---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { createProductAction } from "@/utils/actions/formProduct";
+import { faker } from "@faker-js/faker";
+
+function CreateProductPage() {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
+
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">create product</h1>
+      <div className="border p-8 rounded-md">
+        <form action={createProductAction}>
+          <div className="mb-2">
+            <Label htmlFor="name">Product Name</Label>
+            <Input id="name" name="name" type="text" defaultValue={name} />
+          </div>
+          <Button type="submit" size="lg">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default CreateProductPage;
