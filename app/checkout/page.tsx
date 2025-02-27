4d27bfbe2b98f3b8a4d53c7699270ce5948d9a6d
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-27 22:27:12 +0700

    refactor: checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { Suspense, useCallback } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   EmbeddedCheckoutProvider,
@@ -12,7 +12,7 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 );
 
-export default function CheckoutPage() {
+function CheckoutContent() {
   const searchParams = useSearchParams();
 
   const orderId = searchParams.get("orderId");
@@ -36,10 +36,18 @@ export default function CheckoutPage() {
   const options = { fetchClientSecret };
 
   return (
+    <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+}
+
+export default function CheckoutPage() {
+  return (
     <div id="checkout">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-        <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
+      <Suspense fallback={<div>Loading checkout...</div>}>
+        <CheckoutContent />
+      </Suspense>
     </div>
   );
 }
