e59bc29208c48507d57bf118c3d67407d6ebf236
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-27 22:27:47 +0700

    refactor: form container + user icon

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -26,12 +26,6 @@ function FormContainer({ action, children }: FormContainerProps) {
     }
   }, [state, toast]);
 
-  useEffect(() => {
-    if (state.message) {
-      toast({ description: state.message });
-    }
-  }, [state]);
-
   return <form action={formAction}>{children}</form>;
 }
 
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,6 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { LuUser } from "react-icons/lu";
+import Image from "next/image";
 
 async function UserIcon() {
   const user = await currentUser();
@@ -7,7 +8,13 @@ async function UserIcon() {
 
   if (profileImage) {
     return (
-      <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+      <Image
+        src={profileImage}
+        width={24}
+        height={24}
+        alt="User Profile"
+        className="rounded-full object-cover"
+      />
     );
   }
   return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
