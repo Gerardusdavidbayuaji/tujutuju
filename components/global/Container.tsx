4aafde1e9b147e1222203294b4313e6c3b2eb611
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-03-04 22:51:56 +0700

    fix: container

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -1,13 +1,12 @@
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
-function Container({
-  children,
-  className,
-}: {
-  children: ReactNode;
-  className?: string;
-}) {
+interface ContainerProps {
+  children: string;
+  className: ReactNode;
+}
+
+function Container({ children, className }: ContainerProps) {
   return (
     <div className={cn(`mx-auto max-w-6xl xl:max-w-7xl px-8`, className)}>
       {children}
